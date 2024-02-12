974b6778d8609a82f3ac395884b6f97d41cc725c
Harsh Kumar <harsh@dgpadmins-MacBook-Pro-2.local>
2024-02-11 22:11:02 -0500

    added url fetch for mturk metadata

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState, createContext } from 'react';
+import { useState, createContext, useEffect } from 'react';
 import Controls from './Controls';
 import Absent from './AUT/Absent';
 import Generate from './AUT/Generate';
@@ -19,25 +19,46 @@ function App() {
   const addData = (newData) => {
     setData((prevArray) => [...prevArray, newData]);
   }
+
+  const [acceptedHIT, setAcceptedHIT] = useState(false);
+
+  useEffect(() => {  
+		const urlParams = new URLSearchParams(window.location.search);  
+		const assignmentId = urlParams.get("assignmentId") || "test";
+		setAcceptedHIT(assignmentId !== "ASSIGNMENT_ID_NOT_AVAILABLE");  
+		const hitId = urlParams.get("hitId") || "test";  
+		const turkSubmitTo = urlParams.get("turkSubmitTo") || "test";  
+		const workerId = urlParams.get("workerId") || "test" + Math.floor(Math.random() * 10000);  
+
+		console.log(assignmentId, hitId, turkSubmitTo, workerId);  
+	  }, []); 
   
-  return (
-    <div className="App">
-      <DataContext.Provider value={{data, addData}}>
-        <Router>
-          <Routes>
-            <Route path="/creativity/" element={<Introduction />} />
-            <Route path="/creativity/consent" element={<Consent />} />
-            <Route path="/creativity/controls" element={<Controls />} />
-            <Route path="/creativity/absent" element={<Absent />} />
-            <Route path="/creativity/generate" element={<Generate />} />
-            <Route path="/creativity/refine" element={<Refine />} />
-            <Route path="/creativity/feedback" element={<Feedback />} />
-            <Route path="/creativity/game" element={<Game />} />
-          </Routes>
-        </Router>
-      </DataContext.Provider>
-    </div>
-  );
+    return (
+      <div className="App">
+        {!acceptedHIT ? (
+          <div>
+            In this study, you will be asked to provide alternative uses for commonly found items. 
+            We estimate this task will take around 15 minutes. You must accept this HIT to continue.
+          </div>
+        ) : (
+          <DataContext.Provider value={{data, addData}}>
+            <Router>
+              <Routes>
+                <Route path="/creativity/" element={<Introduction />} />
+                <Route path="/creativity/consent" element={<Consent />} />
+                <Route path="/creativity/controls" element={<Controls />} />
+                <Route path="/creativity/absent" element={<Absent />} />
+                <Route path="/creativity/generate" element={<Generate />} />
+                <Route path="/creativity/refine" element={<Refine />} />
+                <Route path="/creativity/feedback" element={<Feedback />} />
+                <Route path="/creativity/game" element={<Game />} />
+              </Routes>
+            </Router>
+          </DataContext.Provider>
+        )}
+      </div>
+    );
+    
 }
 
 export default App;
