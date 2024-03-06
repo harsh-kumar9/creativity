feec760d0dabb6dee0d3bf77fefd2d2ea51aff49
Jon Vincentius <jon.vincentius@mail.utoronto.ca>
2024-03-06 17:53:14 -0500

    replaced intermediate page theme

--- interface/src/Controls.js
+++ interface/src/Controls.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext } from "react";
 import { DataContext } from "./App";
 import { useNavigate } from 'react-router-dom';
+import background from "../assets/blur-background.svg";
 import "./Controls.css";
 
 const Controls = () => {
@@ -18,16 +19,8 @@ const Controls = () => {
   }, [])
 
   return (
-  <div className="h-screen items-center justify-center flex text-3xl font-semibold space-y-8 bg-amber-400">
-    <div className="flex flex-col items-center justify-center rounded-2xl p-24 bg-amber-500">
-      <h1 className="mb-8">ALTERNATIVE USES TASK</h1>
-      <div className="flex flex-row flex-wrap space-x-4 items-center justify-center">
-        {/* <button onClick={handleAbsentClick} className="outline outline-4 w-48 h-16 rounded-lg hover:bg-orange-500">ABSENT</button>
-        <button onClick={handleGenerateClick} className="outline outline-4 w-48 h-16 rounded-lg hover:bg-orange-500">GENERATE</button>
-        <button onClick={handleRefineClick} className="outline outline-4 w-48 h-16 rounded-lg hover:bg-orange-500">REFINE</button> */}
-      </div>
+    <div style={{ backgroundImage: `url(${background})` }} className="text-white h-screen w-screen items-center justify-center space-y-8 p-14 bg-cover">
     </div>
-  </div>
   );
 }
 
